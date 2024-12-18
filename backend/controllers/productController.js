d9e2ff514326fa8de9a7f7fa8ee3674205bec0c1
poornimakumaran <139451899+poornimakumaran@users.noreply.github.com>
2024-12-18 13:38:08 +0530

    Add files via upload

---
+++ backend/controllers/productController.js
@@ -0,0 +1,42 @@
+const Product = require('../models/productModel');
+
+// Fetch all products
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error.' });
+    }
+};
+
+// Create a product
+exports.createProduct = async (req, res) => {
+    try {
+        const newProduct = new Product(req.body);
+        const savedProduct = await newProduct.save();
+        res.status(201).json(savedProduct);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error.' });
+    }
+};
+
+// Update a product
+exports.updateProduct = async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedProduct);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error.' });
+    }
+};
+
+// Delete a product
+exports.deleteProduct = async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.json({ message: 'Product deleted successfully.' });
+    } catch (err) {
+        res.status(500).json({ message: 'Server error.' });
+    }
+};
